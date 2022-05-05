e4f42507b45c7cc19b23a34d145bbca26325d242
Jamil Awada <jamilawada2002@gmail.com>
2022-05-05 21:25:53 +0300

    added WebView class

--- app/src/main/java/com/jamdev/finalexamsguide/WebViewActivity.java
+++ app/src/main/java/com/jamdev/finalexamsguide/WebViewActivity.java
@@ -3,8 +3,11 @@ package com.jamdev.finalexamsguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
