c595d3c238df933199905160e53160357f5bae0f
Jamil Awada <jamilawada2002@gmail.com>
2022-05-05 21:32:15 +0300

    added onBackPressed() function

--- app/src/main/java/com/jamdev/finalexamsguide/WebViewActivity.java
+++ app/src/main/java/com/jamdev/finalexamsguide/WebViewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -18,5 +19,15 @@ public class WebViewActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
