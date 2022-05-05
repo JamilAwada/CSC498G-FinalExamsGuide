26117821b2eeb986b402001e069f2c6f66c6b737
Jamil Awada <jamilawada2002@gmail.com>
2022-05-05 21:29:25 +0300

    completed WebView class

--- app/src/main/java/com/jamdev/finalexamsguide/WebViewActivity.java
+++ app/src/main/java/com/jamdev/finalexamsguide/WebViewActivity.java
@@ -8,10 +8,15 @@ import android.webkit.WebView;
 public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(URL);
     }
 }
