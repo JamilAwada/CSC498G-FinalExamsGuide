f5ecf2b482df6427cc5dee16ca5b0f5d7fa7db8c
Jamil Awada <jamilawada2002@gmail.com>
2022-05-05 22:34:57 +0300

    completed MainActivity

--- app/src/main/java/com/jamdev/finalexamsguide/MainActivity.java
+++ app/src/main/java/com/jamdev/finalexamsguide/MainActivity.java
@@ -2,11 +2,22 @@ package com.jamdev.finalexamsguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout mobileButton;
+    private RelativeLayout parallelButton;
+    private RelativeLayout softwareButton;
+    private RelativeLayout discreteButton;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,78 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        try {
+
+            db = this.openOrCreateDatabase("examsdb", MODE_PRIVATE, null);
+            db.execSQL("CREATE Table IF NOT EXISTS exam (name VARCHAR, url VARCHAR)");
+            // To be ran once and then commented out
+//            db.execSQL("INSERT INTO exam (name, url) VALUES ('Mobile Computing', 'https://developer.android.com/docs')");
+//            db.execSQL("INSERT INTO exam (name, url) VALUES ('Parallel Programming', 'https://www.open-mpi.org/doc/')");
+//            db.execSQL("INSERT INTO exam (name, url) VALUES ('Software Engineering', 'https://www.visual-paradigm.com/guide/uml-unified-modeling-language/what-is-use-case-diagram/')");
+//            db.execSQL("INSERT INTO exam (name, url) VALUES ('Discrete Structures 2', 'https://www.youtube.com/watch?v=dNRDvLACg5Q')");
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        mobileButton = findViewById(R.id.panel_mobile);
+        parallelButton = findViewById(R.id.panel_parallel);
+        softwareButton = findViewById(R.id.panel_software);
+        discreteButton = findViewById(R.id.panel_discrete);
+
+        mobileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = "";
+                Cursor c = db.rawQuery("SELECT url FROM exam WHERE name = \"Mobile Computing\"", null);
+                int n = c.getColumnIndex("url");
+                c.moveToFirst();
+                URL = c.getString(n);
+                Intent goToWebPage = new Intent(getApplicationContext(), WebViewActivity.class);
+                goToWebPage.putExtra("URL", URL);
+            }
+        });
+
+        parallelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = "";
+                Cursor c = db.rawQuery("SELECT url FROM exam WHERE name = \"Parallel Programming\"", null);
+                int n = c.getColumnIndex("url");
+                c.moveToFirst();
+                URL = c.getString(n);
+                Intent goToWebPage = new Intent(getApplicationContext(), WebViewActivity.class);
+                goToWebPage.putExtra("URL", URL);
+            }
+        });
+
+        softwareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = "";
+                Cursor c = db.rawQuery("SELECT url FROM exam WHERE name = \"Software Engineering\"", null);
+                int n = c.getColumnIndex("url");
+                c.moveToFirst();
+                URL = c.getString(n);
+                Intent goToWebPage = new Intent(getApplicationContext(), WebViewActivity.class);
+                goToWebPage.putExtra("URL", URL);
+            }
+        });
+
+        discreteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = "";
+                Cursor c = db.rawQuery("SELECT url FROM exam WHERE name = \"Discrete Structures 2\"", null);
+                int n = c.getColumnIndex("url");
+                c.moveToFirst();
+                URL = c.getString(n);
+                Intent goToWebPage = new Intent(getApplicationContext(), WebViewActivity.class);
+                goToWebPage.putExtra("URL", URL);
+            }
+        });
+
+
     }
 }
